b430c1c1bfa42bf3041c4e1d0cd2e4ea9139be3c
Md Shayon <mdshayon0@gmail.com>
2019-02-15 15:18:06 +0600

    few middleware setup

--- READMD.md
+++ READMD.md
@@ -1,4 +1,4 @@
-# test with card number
+## test with card number
 
 [few card number you can get for testing](https://stripe.com/docs/quickstart)
 4242 4242 4242 4242
@@ -7,3 +7,5 @@
  - payments -> create new payments
  - setting -> activate test mode for only testing
  - business setting -> data -> delete all test data
+
+## checkout box
--- app.js
+++ app.js
@@ -7,6 +7,26 @@ const exphbs = require('express-handlebars');
 
 const app = express();
 
+//handlebars middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+// parse application/json
+app.use(bodyParser.json());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+
+//SET STATIC FOLDER
+app.use(express.static(`${__dirname}/public`));// we can same with path module
+
+
+
+app.get('/', (req, res)=>{
+    res.render('index');
+});
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, ()=>console.log('Server started http://localhost:5000'))
